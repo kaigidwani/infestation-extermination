903f673b22ad27847c56a5102d0fb57237bb328b
jacabbage03 <jmr7859@rit.edu>
2024-09-25 17:34:44 -0400

    Added Bug and Robot classes

---
+++ InfestationExtermination/Assets/Scripts/Bug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterClasses
+{
+    internal class Bug
+    {
+        private int health;
+        private int damage;
+        private int range;
+        private double rateOfFire;
+    }
+}
---
+++ InfestationExtermination/Assets/Scripts/Robot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterClasses
+{
+    internal class Robot
+    {
+        private int health;
+        private int damage;
+        private int range;
+        private int rateOfFire;
+    }
+}
