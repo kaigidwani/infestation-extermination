370ac5d881161f2e4ce967e8023144ef291afdf0
kaigidwani <kpg8244@rit.edu>
2024-09-27 19:56:42 -0400

    Created the AttemptShoot method for the Robot. Added header comment to Enemy manager and created property for EnemiesList.

--- InfestationExtermination/Assets/Scripts/EnemyManager.cs
+++ InfestationExtermination/Assets/Scripts/EnemyManager.cs
@@ -2,12 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// ===============================
+// AUTHOR: Kai Gidwani
+// CREATE DATE: 9/25/24
+// PURPOSE: Manage the enemies using a manager
+// SPECIAL NOTES:
+// ===============================
+// Change History:
+//  9/27/24 - 
+//  9/25/24 - Created
+//==================================
+
 public class EnemyManager : MonoBehaviour
 {
     // Fields
 
     // List of all bug enemies
-    List<Bug> EnemiesList = new List<Bug>();
+    List<Bug> enemiesList = new List<Bug>();
+
+
+    // Properties
+
+    // Gets the enemies list
+    public List<Bug> EnemiesList
+    {
+        get { return enemiesList; }
+    }
 
 
     // Start is called before the first frame update
@@ -20,6 +40,8 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if the health of any enemy in the list is below 0
+
+        // If so, Remove the object then Destroy the object
     }
 }
--- InfestationExtermination/Assets/Scripts/Robot.cs
+++ InfestationExtermination/Assets/Scripts/Robot.cs
@@ -15,6 +15,12 @@ public class Robot : MonoBehaviour
     // How often a turret can shoot
     [SerializeField] private float rateOfFire;
 
+    // Last used time
+    private float lastShotTime;
+
+    // The enemy manager
+    [SerializeField] private EnemyManager enemyManager;
+
 
     // Properties
 
@@ -66,10 +72,48 @@ public class Robot : MonoBehaviour
     void AttemptShoot()
     {
         // Check if cooldown is up with Delta Time
+        if (Time.deltaTime > lastShotTime + rateOfFire)
+        {
+            // If there is at least one enemy alive
+            if (enemyManager.EnemiesList.Count > 0)
+            {
+                // Set the initial closest enemy to the first one
+                Bug closestEnemy = enemyManager.EnemiesList[0];
 
-        // Find the closest enemy within range using the enemyList from the manager
+                // Set the initial closest distance to the first one
+                // This is so we don't need to redo the same math multiple times,
+                // every time we want to compare distances
+                float closestEnemyDist = Vector3.Distance(this.transform.position,
+                    enemyManager.EnemiesList[0].transform.position);
 
-        // Call the Shoot method to attack the nearest enemy
+
+                // Find the closest enemy within range using the enemyList from the manager
+                foreach (Bug currentEnemy in enemyManager.EnemiesList)
+                {
+                    // If the distance to an enemy is within the range
+                    if (Vector3.Distance(this.transform.position, currentEnemy.transform.position) < range)
+                    {
+                        // Saves the distance to the current enemy in the list
+                        float currentDistance = Vector3.Distance(this.transform.position,
+                            currentEnemy.transform.position);
+
+                        // If the current enemy is closer than the previously closest enemy
+                        if (currentDistance < closestEnemyDist)
+                        {
+                            // Set the closest enemy to the current enemy
+                            closestEnemy = currentEnemy;
+
+                            // Set the closest distance to the current distance
+                            closestEnemyDist = currentDistance;
+                        }
+
+                    }
+                }
+
+                // Call the shoot function on the nearest enemy
+                Shoot(closestEnemy);
+            }
+        }
     }
 
     // Shoot an enemy to do damage to it
