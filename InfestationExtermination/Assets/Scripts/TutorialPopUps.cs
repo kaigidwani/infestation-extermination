d2668d83ced6a13613d6aff64d8f1fd28c5d2a8f
JustinHuang3730 <jh3730@rit.edu>
2024-11-18 23:08:42 -0500

    Tutorial Pop Ups

---
+++ InfestationExtermination/Assets/Scripts/TutorialPopUps.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopUps : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+    }
+}
